d5be22bb5be254acf50f59a48f7ae4125be5ee2f
fengzhi7788 <fzfz@admindeiMac-62.local>
2016-03-21 20:10:38 +0800

    区别在哪里＝＝＃

--- cal/ViewController.swift
+++ cal/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var z: UITextField!
 
     @IBAction func add(sender: AnyObject) {
+        var a:Double=0
+        var b:Double!=0
+        var c:Double=0
+        /*if(!x.text!.isEmpty)
+        {
+        a=(x.text! as NSString).doubleValue
+        }
+        if(!y.text!.isEmpty)
+        {
+        b=(y.text! as NSString).doubleValue
+        }
+        if(!z.text!.isEmpty)
+        {
+        c=(z.text! as NSString).doubleValue
+        }*/
+        if(!x.text!.isEmpty)
+        {
+            a=(x.text! as NSString).doubleValue
+        }
+        if(!y.text!.isEmpty){
+            b=(Double)(y.text!)
+        }
+        c=a+b
+        z.text="\(c)"
+
     }
     override func viewDidLoad() {
         super.viewDidLoad()
