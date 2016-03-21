56d4e681c367cb5a9feb7e30cab10f1c5fc490da
fengzhi7788 <fzfz@admindeiMac-62.local>
2016-03-21 20:46:09 +0800

    按钮0～9可以显示到textfile上了

--- cal/ViewController.swift
+++ cal/ViewController.swift
@@ -9,16 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var x: UITextField!
     @IBOutlet weak var y: UITextField!
     @IBOutlet weak var z: UITextField!
     @IBOutlet weak var xianshi: UITextField!
+    
     @IBAction func one(sender: AnyObject) {
         xianshi.text="1"
-    
+        //xianshi.textColor = [UIColor, red]
         
     }
-
+    
+    @IBAction func two(sender: UIButton) {
+        xianshi.text="2"
+    }
+    @IBAction func three(sender: UIButton) {
+        xianshi.text="3"
+    }
+    @IBAction func four(sender: UIButton) {
+        xianshi.text="4"
+    }
+    @IBAction func five(sender: UIButton) {
+        xianshi.text="5"
+    }
+    @IBAction func six(sender: UIButton) {
+        xianshi.text="6"
+    }
+    @IBAction func seven(sender: UIButton) {
+        xianshi.text="7"
+    }
+    @IBAction func eight(sender: UIButton) {
+        xianshi.text="8"
+    }
+    @IBAction func nine(sender: UIButton) {
+        xianshi.text="9"
+    }
+    @IBAction func zero(sender: UIButton) {
+        xianshi.text="0"
+    }
+    @IBAction func AC(sender: UIButton) {
+        xianshi.text="  "
+    }
     @IBAction func add(sender: AnyObject) {
         var a:Double=0
         var b:Double!=0
