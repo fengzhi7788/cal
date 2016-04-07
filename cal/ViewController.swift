5d55cf4c405768915ae8765147f1bdd760f4942d
fengzhi7788 <fzfz@admindeiMac-62.local>
2016-04-07 11:27:18 +0800

    小数点可以不无限点了
    加了一个bool类型，然后定义有点为false。也就是定义没有点

--- cal/ViewController.swift
+++ cal/ViewController.swift
@@ -9,18 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var date = NSDate()
-    var timeFormatter = NSDateFormatter()
-    //timeFormatter.dateFormat = "yyy-MM-dd 'at' HH:mm:ss.SSS"
-    //var strNowTime = timeFormatter.stringFromDate(date) as String
-    var dianjishu = 0
+
     func dianan(fig:String)
     {
+        
         xianshi.text! += fig
-        //xianshi.text = xianshihuancun
     }
 
-
+    var point=false
     var fuhao = ""
     var value1:Double=0
     var value2:Double=0
@@ -28,73 +24,83 @@ class ViewController: UIViewController {
     @IBOutlet weak var xianshi: UITextField!
     
     @IBAction func one(sender: AnyObject) {
-        //xianshi.text="1"
-        //xianshi.textColor = [UIColor, red]
+       
         dianan("1")
         
     }
     
     @IBAction func two(sender: UIButton) {
         dianan("2")
-        //xianshi.text="2"
+        
     }
     @IBAction func three(sender: UIButton) {
         dianan("3")
-        //xianshi.text="3"
+       
     }
     @IBAction func four(sender: UIButton) {
         dianan("4")
-        //xianshi.text="4"
+        
     }
     @IBAction func five(sender: UIButton) {
         dianan("5")
-        //xianshi.text="5"
+        
     }
     @IBAction func six(sender: UIButton) {
         dianan("6")
-        //xianshi.text="6"
+        
     }
     @IBAction func seven(sender: UIButton) {
         dianan("7")
-        //xianshi.text="7"
+     
     }
     @IBAction func eight(sender: UIButton) {
         dianan("8")
-        //xianshi.text="8"
+       
     }
     @IBAction func nine(sender: UIButton) {
         dianan("9")
-        //xianshi.text="9"
+   
     }
     @IBAction func zero(sender: UIButton) {
         dianan("0")
-        //xianshi.text="0"
+    
     }
     @IBAction func point(sender: UIButton) {
-        
-        dianan(".")
+        if xianshi.text==""
+        {
+            xianshi.text=xianshi.text!+""
+        }
+        else if !point{
+            xianshi.text = xianshi.text!+"."
+            point=true
+        }
     }
     @IBAction func add(sender: UIButton) {
         fuhao="add"
         value1=(xianshi.text! as NSString).doubleValue
         xianshi.text=""
+        point=false
     }
 
     @IBAction func dec(sender: UIButton) {
         fuhao="jian"
         value1=(xianshi.text! as NSString).doubleValue
         xianshi.text=""
+        point=false
     }
     @IBAction func mul(sender: UIButton) {
         fuhao="cheng"
         value1=(xianshi.text! as NSString).doubleValue
         xianshi.text=""
+        point=false
     }
     @IBAction func div(sender: UIButton) {
         fuhao="chu"
         value1=(xianshi.text! as NSString).doubleValue
         xianshi.text=""
+        point=false
     }
+
     @IBAction func equ(sender: UIButton) {
         if(fuhao=="add"){
             value2=(xianshi.text! as NSString).doubleValue
